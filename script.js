8ca7b058e7159bc09e3269a4e573175d1006e9bc
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-09-28 15:20:36 -0500

    first push

---
+++ script.js
@@ -0,0 +1,61 @@
+// 1
+let warmHugs = "Hi \, I\'m Olaf and I like warm hugs.";
+
+// 1b
+console.log(warmHugs.toUpperCase());
+
+// 1c
+console.log(warmHugs.replace("like", "LOVE"));
+
+// 2a
+var beenImpaled = "Oh, look at that. I\'ve been impaled."
+console.log(beenImpaled)
+
+// 2b
+console.log(beenImpaled.slice(18))
+
+// 3a
+var dotDotDot = "..."
+
+//3b
+skullBones = `I don't have a skull ${dotDotDot} or bones`;
+
+// 4
+console.log(Math.PI);
+
+// 5
+
+// theres gotta be a better way to do this but for some reason when i try to simplify it I cant get it to work. reminder to ask tommorow.
+
+let randomNumber = Math.random();
+
+randomNumber *= 3;
+
+randomNumber=Math.floor(randomNumber)
+
+randomNumber++;
+
+console.log(randomNumber)
+
+// Bonus
+
+// 6
+
+// I cant figure out how to do this one without a variable :/
+
+// console.log" Let it go!"(string.repeat(2))(snow.toUpperCase)" Let it go!"
+
+
+// 7 a
+let reindeer="Reindeer are better than people."
+
+// 7b
+console.log(reindeer.replaceAll(" ", "_"));
+
+// 8
+console.log(Math.sqrt(2));
+
+// 9
+let newRandomNumber = Math.floor(Math.random() * 15)+7
+
+console.log(newRandomNumber)
